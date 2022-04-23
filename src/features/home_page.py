2d36f5385145be45dd4d5494095c337c7ea6b085
Tub1s <senukiq@gmail.com>
2022-04-24 01:33:07 +0200

    added multi-choice support; restructurization of app; imported juxtapose component

--- src/features/home_page.py
+++ src/features/home_page.py
@@ -2,6 +2,7 @@ import streamlit as st
 import matplotlib.pyplot as plt
 
 def show_home_page():
+    # Juxtapose https://github.com/robmarkcole/streamlit-image-juxtapose
     st.title("Welcome to the Super Resolution Application!")
     st.write("This application lets you upload up to 200MB of pictures and upscale them using multitude of techniques and algorithms!")
     st.write("List of currently available algorithms is as follows:")
--- src/features/main_app_page.py
+++ src/features/main_app_page.py
@@ -1,14 +1,10 @@
 import streamlit as st
+from features.streamlit_juxtapose import juxtapose
 from features.scaling import upscale
 from cv2 import INTER_LINEAR, INTER_CUBIC, INTER_NEAREST, INTER_LANCZOS4
 from PIL import Image 
 
 def show_main_app_page():
-    st.title("Application page")
-
-    col1, col2 = st.columns(2)
-    list_of_file_names = list()
-    
     IMAGE_TYPES_LIST = [
     '.jpg', '.jpeg', '.png', '.gif', '.webp', '.bmp'
     ]
@@ -18,7 +14,13 @@ def show_main_app_page():
                           'Nearest Neighbour': INTER_NEAREST, 
                           'Lanczos4': INTER_LANCZOS4}
 
-    with col1:
+    st.title("Application page")
+
+    list_of_file_names = list()
+    show_results = False
+    
+
+    with st.container():
         with st.expander("Import data"):
             uploaded_files = st.file_uploader(label = "Choose image files:", type = IMAGE_TYPES_LIST, accept_multiple_files=True)
             list_of_file_names = [file.name for file in uploaded_files]
@@ -27,15 +29,29 @@ def show_main_app_page():
                 files = st.multiselect("Choose files that you want to process: ", list_of_file_names)
                 
                 if files:
-                    choice = st.multiselect("Choose algorithm you want to apply: ", UPSCALING_ALGORITHMS.keys())
-                    upscaling_ratio = st.number_input("Upscaling ratio", min_value=1.0, max_value=4.0)
+                    choices = st.multiselect("Choose algorithm you want to apply: ", UPSCALING_ALGORITHMS.keys())
+                    upscaling_ratio = st.number_input("Upscaling ratio", min_value=1.0, max_value=4.0, step=1.0)
 
-                    if choice:
+                    if choices:
                         uploaded_files = [file for file in uploaded_files if file.name in files]
         
                         if st.button(label='Apply'):
-                            img = upscale(uploaded_files[0], UPSCALING_ALGORITHMS[choice[0]], upscaling_ratio)
-                            #image = Image.open(image)
-                            st.image(img)
-    with col2:
-        st.write("Test")
+                            result_images = {key: dict() for key in choices}
+
+                            
+                            for uploaded_file in uploaded_files:
+                                for choice in choices:
+                                    result_images[choice][uploaded_file.name] = upscale(data = uploaded_file, 
+                                                                    algorithm = UPSCALING_ALGORITHMS[choice], 
+                                                                    upscaling_ratio = upscaling_ratio)
+
+                            st.success('Data have been sucessfully processed!')
+                            show_results = True
+
+
+        # TODO: Add juxtapose scrolling comparisons
+        with st.container():
+            if show_results:
+                for uploaded_file in uploaded_files:
+                    for choice in choices:
+                        st.image(result_images[choice][uploaded_file.name])
